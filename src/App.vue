c24c371a561dd459c25299c5b427106b522a1b14
Daniel <dijalmaduartefleitas0@gmail.com>
2026-08-15 20:37:48 -0400

    Adicionado modais,responsividade e padronização de estilos css

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import HeroSection from './components/HeroSection.vue'
+</script>
+
+<template>
+  <div class="home-container">
+    <HeroSection />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.home-container {
+  font-family: sans-serif;
+}
+</style>
+
+<style>
+body {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+</style>
