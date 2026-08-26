ab34a45251ded22a83dbc57f655928596575e1de
Daniel <dijalmaduartefleitas0@gmail.com>
2026-08-26 09:20:56 -0400

    Adicionando novo caminho para cor

--- src/components/googleButton.vue
+++ src/components/googleButton.vue
@@ -24,7 +24,7 @@
                     method:"Post",
                     headers:{"Content-Type":"application/json"},
                     body:JSON.stringify({
-                        token:response,
+                        token:response.credential,
                     })
                 })
                 
--- src/server/server.js
+++ src/server/server.js
@@ -16,6 +16,7 @@ const allowedOrigins = [
     "http://localhost:5500",
     "http://127.0.0.1:5500",
     "http://localhost:5174",
+    "http://localhost:5173",
     "http://localhost:3000",
     "https://echo-moda-2-0.vercel.app"
 ];
